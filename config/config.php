3efb2ddbf93b51b8891adf5611543277a96fc5e0
Coder 79 <dev.ahmed.saeed@gmail.com>
2021-09-28 06:17:19 +0200

    update INSTALLED 0

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 define("DBHOST", "localhost");
 define("DBUSER", "root");
 define("DBPASS", "");
-define("DBNAME", "cart");
-define("SITEURL", "http://localhost/Takehome");
-define("INSTALLED", 1);
+define("DBNAME", "");
+define("SITEURL", "");
+define("INSTALLED", 0);
 ?>
